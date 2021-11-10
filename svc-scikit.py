c2e8baf7f3c6d0d13eb6f2eadf5b658ee0715b80
Shirley Xu <shirley.t.xu@gmail.com>
2021-11-09 20:06:40 -0800

    add SVC confusion matrix

--- svc-scikit.py
+++ svc-scikit.py
@@ -1,28 +1,44 @@
 import time
-import numpy as np
 import pandas as pd
+from sklearn.metrics import plot_confusion_matrix, confusion_matrix, \
+    ConfusionMatrixDisplay
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 
+# reads in dataset
 df = pd.read_csv('breastcancerdataset.csv')
+
+# dataframe includes unnamed columns
+# accesses all columns that are unnamed and removes them
 df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
-# set target
+# sets target to column titled "diagnosis"
 y = df.loc[:,["diagnosis"]]
+
+# deletes diagnosis column from dataframe
 del df["diagnosis"]
 
-# data 70-30 split
+# data split 70-30
 trainX, testX, trainY, testY = train_test_split(df, y, test_size=0.3)
+
+# makes testY and trainY into 1D array as expected by predict and train functions
+testY = testY.values.ravel()
+trainY = trainY.values.ravel()
+
+# scales data to 0-1 range
+# including a scaler has shown to improve network accuracy by over 30%
 scaler = MinMaxScaler()
-# transform data
+
+# transforms data
+# target y does not need to be scaled because it is binary ("b" or "m")
 trainX = scaler.fit_transform(trainX)
 testX = scaler.fit_transform(testX)
 
+"""
 # visualize some parameters of data
 xAxis = df.loc[:,['radius_mean']]
 plt.hist(xAxis)
@@ -50,22 +66,29 @@ plt.title("Perimeter Mean Histogram")
 plt.show(block=False)
 plt.pause(2)
 plt.close()
+"""
 
 # svc network
 svc = SVC(verbose=0, max_iter=10000)
 
 # training
 startTraining = time.time()
-svc.fit(trainX, trainY.values.ravel())
+svc.fit(trainX, trainY)
 stopTraining = time.time()
 svcTrainingTime = stopTraining-startTraining
 print("SVC Training Time:", svcTrainingTime)
 
-# testing
 startTest = time.time()
-svcPreds = svc.score(testX, testY)
+svcPreds = svc.predict(testX)
 stopTest = time.time()
 svcTestingTime = stopTest-startTest
+
+svcConfusionMatrix = confusion_matrix(testY, svcPreds, labels=["B", "M"])
+display = ConfusionMatrixDisplay(confusion_matrix=svcConfusionMatrix, display_labels=svc.classes_)
+display.plot()
+plt.title("SVC Neural Network Confusion Matrix")
+plt.show()
+
 print("SVC Testing Time: ", svcTestingTime)
 print("SVC Accuracy:     ", svcPreds)
 print("")
@@ -75,7 +98,7 @@ logreg = LogisticRegression()
 
 # training
 startTraining = time.time()
-logreg.fit(trainX, trainY.values.ravel())
+logreg.fit(trainX, trainY)
 stopTraining = time.time()
 logregTrainingTime = stopTraining-startTraining
 print("LogReg Training Time:", logregTrainingTime)
@@ -94,7 +117,7 @@ kneighclassifier = KNeighborsClassifier()
 
 # training
 startTraining = time.time()
-kneighclassifier.fit(trainX, trainY.values.ravel())
+kneighclassifier.fit(trainX, trainY)
 stopTraining = time.time()
 kneighTrainingTime = stopTraining-startTraining
 print("KNeighborsClassifier Training Time:", kneighTrainingTime)
